242463905804be8b9d11b63058b97cb3bda587ac
OraKin <elminor2002@gmail.com>
2024-07-12 11:44:01 +0100

    Switch testing

--- Codes/switch.py
+++ Codes/switch.py
@@ -2,11 +2,13 @@ from gpiozero import LED, Button
 from time import sleep
 
 led1 = LED(25)
-button = Button(2)
+# button = Button(2)
+button_state = False  # Initial state of the button
 
 while True:
 
-    if button.is_pressed:
+    # if button.is_pressed:
+    if button_state:
         led1.on()
         if led1.is_lit:
             exec(open("mqttexampleLOCAL.py").read())
@@ -18,4 +20,7 @@ while True:
             exec(open("mqttexampleLOCAL2.py").read())
         else:
             print("There is an error")
-    sleep(1)
+            
+    # Simulate button press/release
+    button_state = not button_state
+    sleep(2)  # Change button state every 2 seconds
