07d9c36f11bd187507ee7fcf2052ba424d9a1a90
OraKin <elminor2002@gmail.com>
2024-07-29 10:42:56 +0100

    Major Django changes

--- WebPageDjango/WebPageDjango/settings.py
+++ WebPageDjango/WebPageDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add our new application
+    'smarthome.apps.SmarthomeConfig', # This object was created for us in /smarthome/apps.py
 ]
 
 MIDDLEWARE = [
--- WebPageDjango/WebPageDjango/urls.py
+++ WebPageDjango/WebPageDjango/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# Use include() to add paths from the smarthome application
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('smarthome/', include('smarthome.urls')),
 ]
+# Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='smarthome/', permanent=True)),
+]
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
