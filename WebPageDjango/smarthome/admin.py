6ce6e6966c2ac1e690ae3df99d147810072139b2
OraKin <elminor2002@gmail.com>
2024-07-29 11:30:31 +0100

    Django changes

--- WebPageDjango/smarthome/admin.py
+++ WebPageDjango/smarthome/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Room, Device, Sensor, Setting
 
-# Register your models here.
+class DeviceInline(admin.TabularInline):
+    model = Device
+    extra = 0
+
+class SensorInline(admin.TabularInline):
+    model = Sensor
+    extra = 0
+
+class SettingInline(admin.TabularInline):
+    model = Setting
+    extra = 0
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [DeviceInline]
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'room', 'is_active')
+    list_filter = ('room', 'is_active')
+    search_fields = ('name', 'description')
+    inlines = [SensorInline, SettingInline]
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'device', 'value', 'last_updated')
+    list_filter = ('device',)
+    search_fields = ('name', 'description')
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'device', 'value')
+    list_filter = ('device',)
+    search_fields = ('name', 'description')
--- WebPageDjango/smarthome/models.py
+++ WebPageDjango/smarthome/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, related_name='devices')
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('device_detail', kwargs={'pk': self.pk})
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='sensors')
+    value = models.FloatField(default=0)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Setting(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='settings')
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
