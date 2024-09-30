68979e0d6201ec1aa4e68a6e8d6be4cbae320be7
OraKin <elminor2002@gmail.com>
2024-09-30 19:21:51 +0100

    Allowed host https://smarthome-epsu.onrender.com

--- WebPageDjango/WebPageDjango/settings.py
+++ WebPageDjango/WebPageDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['smarthome-ailab.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['smarthome-epsu.onrender.com','127.0.0.1']
 
 # Application definition
 
