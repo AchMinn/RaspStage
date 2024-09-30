24fd5609d3557eb6eb4860e3ddfd9af296eba3cc
OraKin <elminor2002@gmail.com>
2024-09-30 20:18:59 +0100

    Final changes for local deployement

--- WebPageDjango/WebPageDjango/settings.py
+++ WebPageDjango/WebPageDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['smarthome-epsu.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 # Application definition
 
@@ -136,11 +136,11 @@ SECURE_HSTS_PRELOAD = True  # Optional
 
 # Redirect all HTTP traffic to HTTPS
 
-SECURE_SSL_REDIRECT = True 
+# SECURE_SSL_REDIRECT = True 
 
 # Ensure session cookies are only sent over HTTPS
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
 # Make the CSRF cookie secure
 
