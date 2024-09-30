aa35e7bd421bc03d63f1c45fa028bb7e4e1d707d
OraKin <elminor2002@gmail.com>
2024-09-30 18:49:06 +0100

    Env File Added

--- WebPageDjango/WebPageDjango/settings.py
+++ WebPageDjango/WebPageDjango/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l7%&7va(7!y9b!kk2ops6ijxod#$ysz)wz8u+$%_2)2#!f&6-2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -78,11 +80,11 @@ WSGI_APPLICATION = 'WebPageDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smarthome_db',  # Your database name
-        'USER': 'databaseuser',   # Your PostgreSQL username
-        'PASSWORD': 'databasepassword',  # Your PostgreSQL password
-        'HOST': 'localhost',  # Set to 'db' if using Docker
-        'PORT': '',           # Default is 5432
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),  # Default to 5432 if not set
     }
 }
 
